ff56ec8c882cf2595ffdef842488599f85ffa571
steven hurwitt <steven.hurwitt@bp.com>
2022-06-29 15:05:32 -0500

    add dockerfiles

---
+++ src/main/python/main.py
@@ -0,0 +1,14 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from kafka import kafkaProducer, kafkaConsumer
+from twitter_ingestion import *
+import datetime as dt
+import json
+import os
+
+def main():
+    print("function goes here...")
+
+if __name__ == "__main__":
+    main()
