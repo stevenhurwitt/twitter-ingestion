06eb9968627e02a08cc26cd4df71b7042e20dfa5
steven hurwitt <stevenhurwitt@gmail.com>
2022-11-01 04:04:03 -0500

    python 3.7

--- src/main/python/twitter-main/main.py
+++ src/main/python/twitter-main/main.py
@@ -8,16 +8,31 @@ import datetime as dt
 import json
 import os
 
+# read creds json
+
 with open("creds.json", "r") as f:
     creds = json.load(f)
     f.close()
     print("read creds.json")
+
+# main funciton
 
 def main():
     print("starting...")
     os.environ["AWS_ACCESS_KEY_ID"] = creds["aws-client"]
     os.environ["AWS_SECRET_ACCESS_KEY"] = creds["aws-secret"]
     twitter_ingestion()
+
+# twitter ingestion
+
+def twitter_ingestion():
+    print("twitter ingestion...")
+    header = {}
+    df = spark.read.csv("file.csv", header = True)
+
+    pass
+
+###### run main function #####
 
 if __name__ == "__main__":
     main()
