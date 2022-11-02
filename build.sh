df275c388c49253c5a28364246849e384f40aa7c
steven hurwitt <stevenhurwitt@gmail.com>
2022-11-01 20:07:39 -0500

    modify requirements pandas 1.3.5

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-SPARK_VERSION="3.2.0"
+SPARK_VERSION="3.2.2"
 HADOOP_VERSION="3.2"
 JUPYTERLAB_VERSION="3.2.5"
 
 # -- Building the Images
 
-docker build -f cluster-base.Dockerfile -t stevenhurwitt/cluster-base:latest .
+docker build -f cluster-base.Dockerfile -t cluster-base .
 
-docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg hadoop_version="${HADOOP_VERSION}" -f spark-base.Dockerfile -t stevenhurwitt/spark-base:latest .
+docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg hadoop_version="${HADOOP_VERSION}" -f spark-base.Dockerfile -t spark-base .
 
-docker build -f spark-master.Dockerfile -t stevenhurwitt/spark-master:latest .
+docker build -f spark-master.Dockerfile -t spark-master .
 
-docker build -f spark-worker.Dockerfile -t stevenhurwitt/spark-worker:latest .
+docker build -f spark-worker.Dockerfile -t spark-worker .
 
-docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" -f jupyterlab.Dockerfile -t stevenhurwitt/jupyterlab:latest .
+docker build --build-arg spark_version="${SPARK_VERSION}" --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" -f jupyterlab.Dockerfile -t jupyterlab .
 
